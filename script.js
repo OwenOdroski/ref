8c4843b0ddbd720cf1685569fd434c01ee094e39
OwenOdroski <116469702+OwenOdroski@users.noreply.github.com>
2026-06-30 06:16:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -194,7 +194,6 @@ async function checkJSON() {
       if(user.toUpperCase() == "CBAKER2026") {
         alert('cunt')
       }
-      alert(user)
 
       window.setTimeout(async function() {
         const text = await file.text();   // Read file as string
